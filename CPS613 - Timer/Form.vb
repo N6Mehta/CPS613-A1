36bac1727edea34fe2381460c57b15db928b07e6
n6mehta <nitish.mehta33@gmail.com>
2016-10-08 11:47:21 -0400

    update

--- CPS613 - Timer/Form.vb
+++ CPS613 - Timer/Form.vb
@@ -1,13 +1,3 @@
 ﻿Public Class Center
-    Private Sub SingleTimer1_Load(sender As Object, e As EventArgs)
 
-    End Sub
-
-    Private Sub SingleTimer1_Load_1(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Sec_Click(sender As Object, e As EventArgs) Handles Sec.Click
-
-    End Sub
 End Class
